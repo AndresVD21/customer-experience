6525e70a941e95a0d2a0163a1942b29d9e065001
Andres <andresvd21@gmail.com>
2022-10-24 20:54:16 -0500

    feat(shared/styles): adding status pill component + adding color variables

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { StatusPillComponent } from './components/status-pill/status-pill.component';
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    StatusPillComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    StatusPillComponent
   ]
 })
 export class SharedModule { }
