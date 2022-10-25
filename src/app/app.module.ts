28fe83168467141d1af3595733deda0aba32dffa
Andres <andresvd21@gmail.com>
2022-10-24 20:38:11 -0500

    feat(app): integrating new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerListItemComponent } from './components/customer-list/customer-list-item/customer-list-item.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerListComponent,
+    CustomerListItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
