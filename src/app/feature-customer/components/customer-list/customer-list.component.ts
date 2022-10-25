6ec6336486c10f9ce5d209df44d928b3f055d344
Andres <andresvd21@gmail.com>
2022-10-25 12:31:35 -0500

    feat(feature-customer): wiring customer list with state

--- src/app/feature-customer/components/customer-list/customer-list.component.ts
+++ src/app/feature-customer/components/customer-list/customer-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../../models/customer';
 import { v4 as uuid } from 'uuid';
+import { select, Store } from '@ngrx/store';
+import { IAppState } from 'src/app/store/state/app.state';
+import { selectCustomers } from 'src/app/store/selectors/customer.selectors';
 
 @Component({
   selector: 'app-customer-list',
@@ -8,6 +11,8 @@ import { v4 as uuid } from 'uuid';
   styleUrls: ['./customer-list.component.scss']
 })
 export class CustomerListComponent implements OnInit {
+
+  customers$ = this.store.pipe(select(selectCustomers));
 
   customerList: Customer[] = [
     new Customer(uuid(), 'Joe', 'Doe', 'active', 'joe@ce.com'),
@@ -17,7 +22,7 @@ export class CustomerListComponent implements OnInit {
     new Customer(uuid(), 'Jack', 'Doe', 'pending', 'jack@ce.com'),
   ];
 
-  constructor() { }
+  constructor(private store: Store<IAppState>) { }
 
   ngOnInit(): void {
   }
