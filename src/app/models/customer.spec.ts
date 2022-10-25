82d97b3fa3f44825aac63631c9c5fa1bc06a517e
Andres <andresvd21@gmail.com>
2022-10-24 21:47:14 -0500

    feat: removing models outter dir

--- src/app/models/customer.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Customer } from './customer';
-
-describe('Costumer', () => {
-  it('should create an instance', () => {
-    expect(new Customer('000', 'test', 'test', 'active', 'email')).toBeTruthy();
-  });
-});
