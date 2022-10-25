7e8c6d2d70446c52f881ffe366387bd7e8009d5e
Andres <andresvd21@gmail.com>
2022-10-24 21:48:26 -0500

    update(app): updating app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerListComponent } from './components/customer-list/customer-list.component';
-import { CustomerListItemComponent } from './components/customer-list/customer-list-item/customer-list-item.component';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerListComponent,
-    CustomerListItemComponent,
   ],
   imports: [
     BrowserModule,
