a7851e79dcff104142a1a19d64ef9a7099cad1bd
Andres <andresvd21@gmail.com>
2022-10-25 18:24:12 -0500

    feat(feature-customer): adding new customer functionality

--- src/app/feature-customer/components/customer-details/customer-details.component.ts
+++ src/app/feature-customer/components/customer-details/customer-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import * as fromCustomer from 'src/app/store/actions/customer.actions';
 import { selectCustomer } from 'src/app/store/selectors/customer.selectors';
@@ -8,6 +8,8 @@ import { Customer } from '../../models/customer';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil, map } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { v4 as uuid } from 'uuid';
+import { formatPhone, revertPhoneFormat } from 'src/app/utils/utils';
 
 @Component({
   selector: 'app-customer-details',
@@ -15,6 +17,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./customer-details.component.scss'],
 })
 export class CustomerDetailsComponent implements OnInit, OnDestroy {
+
+  NEW_PARAM = 'new';
+
   private destroy$ = new Subject<boolean>();
 
   customer$: Observable<Customer>;
@@ -23,19 +28,27 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy {
   editMode = false;
   customerForm: FormGroup;
 
+  isNewCustomer = true;
+
   constructor(
     private store: Store<IAppState>, 
     private route: ActivatedRoute,
+    private router: Router,
     private fb: FormBuilder,
   ) {}
 
   ngOnInit(): void {
     const customerId = this.route.snapshot.paramMap.get('id');
-    if (customerId) {
-      this.store.dispatch(fromCustomer.GetCustomer({ id: customerId }));
+    if (customerId !== this.NEW_PARAM) {
+      this.isNewCustomer = false;
+      if (customerId) {
+        this.store.dispatch(fromCustomer.GetCustomer({ id: customerId }));
+      }
+      this.setCustomer();
+    } else {
+      this.editMode = true;
     }
 
-    this.setCustomer();
     this.initForm();
   }
 
@@ -44,6 +57,7 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy {
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       status: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern("^[0-9]{10}$")]],
       email: ['',[ Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]]
     })
   }
@@ -52,6 +66,7 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy {
     this.customerForm.setValue({
       firstName: customer.firstName,
       lastName: customer.lastName,
+      phone: revertPhoneFormat(customer.phone),
       email: customer.email,
       status: customer.status
     })
@@ -73,11 +88,20 @@ export class CustomerDetailsComponent implements OnInit, OnDestroy {
   }
 
   saveData() {
-    const { firstName, lastName, status, email } = this.customerForm.value;
-    const customer = new Customer(this.customer.id, firstName, lastName, status, email, this.customer.phone);
-    this.store.dispatch(fromCustomer.EditCustomer({customer}));
-    this.editMode = false;
-    this.store.dispatch(fromCustomer.GetCustomer({ id: this.customer.id }));
+    const { firstName, lastName, status, email, phone } = this.customerForm.value;
+    const phoneFormatted = formatPhone(phone);
+    if (!this.isNewCustomer) {
+      const customer = new Customer(this.customer.id, firstName, lastName, status, email, phoneFormatted);
+      this.store.dispatch(fromCustomer.EditCustomer({customer}));
+      this.editMode = false;
+      this.store.dispatch(fromCustomer.GetCustomer({ id: this.customer.id }));
+    } else {
+      const newId = uuid();
+      const customer = new Customer(newId, firstName, lastName, status, email, phoneFormatted);
+      this.store.dispatch(fromCustomer.AddCustomer({customer}));
+      this.router.navigate(['/'])
+      
+    }
     this.customerForm.reset();
   }
 
