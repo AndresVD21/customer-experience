590375bba328155ba497fe1e9bb5278bee447a61
Andres <andresvd21@gmail.com>
2022-10-25 12:30:55 -0500

    feat(app): getting users + filling local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from './feature-customer/models/customer';
+import { v4 as uuid } from 'uuid';
+import { UsersService } from './services/users.service';
+import { Store } from '@ngrx/store';
+import * as fromCustomer from './store/actions/customer.actions';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'customer-experience';
+
+  customers: Customer[];
+
+  constructor(private usersService: UsersService, private store: Store) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  setCustomersLocalStorage(customers: Customer[]) {
+    localStorage.setItem('customers', JSON.stringify(customers));
+  }
+
+  getCustomersFromLocalStorage(): Customer[] {
+    return JSON.parse(localStorage.getItem('customers'));
+  }
+
+  isDataInLocal() {
+    const data = localStorage.getItem('customers');
+    if (data) {
+      return true;
+    }
+    return false;
+  }
+
+  getUsers() {
+    if (this.isDataInLocal()) {
+      const customersStored = this.getCustomersFromLocalStorage();
+      this.store.dispatch(fromCustomer.SetCustomers({customers: customersStored}));
+    } else {
+      this.usersService.getUsers().subscribe(data => {
+        this.setCustomersLocalStorage(data)
+        this.store.dispatch(fromCustomer.SetCustomers({customers: data}));
+      });
+    }
+  }
 }
