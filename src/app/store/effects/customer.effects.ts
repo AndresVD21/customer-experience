9bbcfcbb96285de984b187b7838c995dad52b630
Andres <andresvd21@gmail.com>
2022-10-25 16:04:20 -0500

    feat(store): changing edit logic to be an effect

--- src/app/store/effects/customer.effects.ts
+++ src/app/store/effects/customer.effects.ts
@@ -1,29 +1,48 @@
-import {Injectable} from '@angular/core';
-import {createEffect, Actions, ofType} from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
 import * as fromCustomer from '../actions/customer.actions';
-import {map, withLatestFrom, switchMap} from 'rxjs/operators';
-import {of} from 'rxjs';
+import { map, withLatestFrom, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 import { IAppState } from '../state/app.state';
 import { select, Store } from '@ngrx/store';
 import { selectCustomers } from '../selectors/customer.selectors';
 
 @Injectable()
 export class CustomerEffects {
-    getCustomer$ = createEffect(() => 
-        this.actions$.pipe(
-            ofType(fromCustomer.GetCustomer),
-            map(action => {
-                return action.id
-            }),
-            withLatestFrom(this.store.pipe(select(selectCustomers))),
-            switchMap(([id, customers]) => {
-                const selectedCustomer = customers.filter(customer => customer.id === id)[0];
-                return of(fromCustomer.SetCustomer({customer: selectedCustomer}))
-            })
-        ));
+  getCustomer$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromCustomer.GetCustomer),
+      map((action) => {
+        return action.id;
+      }),
+      withLatestFrom(this.store.pipe(select(selectCustomers))),
+      switchMap(([id, customers]) => {
+        const selectedCustomer = customers.filter(
+          (customer) => customer.id === id
+        )[0];
+        return of(fromCustomer.SetCustomer({ customer: selectedCustomer }));
+      })
+    )
+  );
 
-    constructor(
-        private actions$: Actions,
-        private store: Store<IAppState>
-    ) {}
+  editCustomer$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fromCustomer.EditCustomer),
+      map((action) => {
+        return action.customer;
+      }),
+      withLatestFrom(this.store.pipe(select(selectCustomers))),
+      switchMap(([customer, customers]) => {
+        const customerEditedIndex = customers.findIndex(
+            (costumer) => costumer.id === customer.id
+          );
+          const customersCopy = [...customers];
+          customersCopy.splice(customerEditedIndex, 1, customer);
+          localStorage.setItem('customers', JSON.stringify(customersCopy));
+          return of(fromCustomer.SetCustomers({ customers: customersCopy }));
+      })
+    )
+  );
+
+  constructor(private actions$: Actions, private store: Store<IAppState>) {}
 }
--- src/app/store/reducers/customer.reducers.ts
+++ src/app/store/reducers/customer.reducers.ts
@@ -10,14 +10,6 @@ export const customerReducers = createReducer(
   on(CustomerActions.SetCustomer, (state, props) => {
     return { ...state, customerSelected: { ...props.customer } };
   }),
-  on(CustomerActions.EditCustomer, (state, props) => {
-    const customerEditedIndex = state.customers.findIndex(
-      (costumer) => costumer.id === props.customer.id
-    );
-    const customersCopy = [...state.customers];
-    customersCopy.splice(customerEditedIndex, 1, props.customer);
-    return { ...state, customers: [...customersCopy] };
-  }),
   on(CustomerActions.AddCustomer, (state, props) => {
     return {
       ...state,
