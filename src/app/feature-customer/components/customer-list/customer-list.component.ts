9f077c0f589b25856861385e7d9c812f4379a3c6
Andres <andresvd21@gmail.com>
2022-10-25 17:25:35 -0500

    feat(feature-customer): setting up list filter

--- src/app/feature-customer/components/customer-list/customer-list.component.ts
+++ src/app/feature-customer/components/customer-list/customer-list.component.ts
@@ -1,30 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Customer } from '../../models/customer';
 import { v4 as uuid } from 'uuid';
 import { select, Store } from '@ngrx/store';
 import { IAppState } from 'src/app/store/state/app.state';
 import { selectCustomers } from 'src/app/store/selectors/customer.selectors';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.scss']
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit, OnDestroy {
 
-  customers$ = this.store.pipe(select(selectCustomers));
+  private destroy$ = new Subject<boolean>();
 
-  customerList: Customer[] = [
-    new Customer(uuid(), 'Joe', 'Doe', 'active', 'joe@ce.com'),
-    new Customer(uuid(), 'Jane', 'Doe', 'inactive', 'jane@ce.com'),
-    new Customer(uuid(), 'Jack', 'Doe', 'pending', 'jack@ce.com'),
-    new Customer(uuid(), 'Jane', 'Doe', 'inactive', 'jane@ce.com'),
-    new Customer(uuid(), 'Jack', 'Doe', 'pending', 'jack@ce.com'),
-  ];
+  customers$: Observable<Customer[]>;
+  customers: Customer[];
+  filterCustomers: Customer[];
 
   constructor(private store: Store<IAppState>) { }
 
   ngOnInit(): void {
+    this.setCustomers();
+  }
+
+  setCustomers() {
+    this.customers$ = this.store
+      .pipe(select(selectCustomers));
+    this.customers$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.customers = [...data];
+        this.filterCustomers = [...data]
+      });
+  }
+
+
+  filterCustomerList(filter: string) {
+    console.log(filter)
+    if (filter.trim().length === 0) {
+      this.filterCustomers = [...this.customers];
+      return;
+    }
+    this.filterCustomers = this.customers.filter(customer => customer.lastName.toLowerCase().startsWith(filter))
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
