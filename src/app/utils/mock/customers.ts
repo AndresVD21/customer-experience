a078a0631b37e78fd2f3d39dd5384d6621b9182f
Andres <andresvd21@gmail.com>
2022-10-27 01:39:06 -0500

    feat(utils): adding mock data

---
+++ src/app/utils/mock/customers.ts
@@ -0,0 +1,7 @@
+import { Customer } from "src/app/feature-customer/models/customer";
+
+export const Customers: Customer[] = [
+    {id:'001',firstName: 'ctest1', lastName: 'btest1', email: 'test1@ce.com', status: 'active', phone: '100-000-0000'},
+    {id:'002',firstName: 'btest2', lastName: 'atest2', email: 'test2@ce.com', status: 'pending', phone: '200-000-0000'},
+    {id:'003',firstName: 'atest3', lastName: 'ctest3', email: 'test3@ce.com', status: 'inactive', phone: '300-000-0000'},
+]
