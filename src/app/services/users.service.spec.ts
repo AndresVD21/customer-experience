36d69b8a64a1407bca37aa740163f2fd20d4d6a9
Andres <andresvd21@gmail.com>
2022-10-27 01:39:54 -0500

    feat(testing): updating test cases to increase coverage

--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersService } from './users.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { Customer } from '../feature-customer/models/customer';
+import { Customers } from '../utils/mock/customers';
+
+const expectedUrl = 'https://63575e2e2712d01e14069970.mockapi.io//users';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let controller: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService],
+    });
     service = TestBed.inject(UsersService);
+    controller = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('searches for users', () => {
+    let users: Customer[];
+    service.getUsers().subscribe((data) => {
+      users = data;
+    });
+    const request = controller.expectOne(expectedUrl);
+    request.flush(Customers);
+    expect(users.length).toEqual(3);
   });
 });
